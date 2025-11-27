fe0580ceac2417dbfc4de54e54fe445f0d751900
swapnil0028 <swapnilcodes28@gmail.com>
2025-11-27 21:05:12 +0530

    operations

--- 01/03_convertionOperations.js
+++ 01/03_convertionOperations.js
@@ -1,14 +1,14 @@
 let score = false;
 
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
 // "33" => 33
@@ -21,7 +21,9 @@ console.log(valueInNumber);
 let isLoggedIn = 1
 
 let booleanIsloggedIn = Boolean(isLoggedIn)
-console.log(booleanIsloggedIn);
+// console.log(booleanIsloggedIn);
+
+
 // 1 => true , 0 => false
 // "" => false , "abc" => true
 // null => false
@@ -32,7 +34,40 @@ console.log(booleanIsloggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+ 
+//*****************************Opertaions*********************//
 
 
+
+let value = 3 
+let negvalue = -value 
+
+// console.log(negvalue);
+
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 **3);
+// console.log(2 % 2);
+// console.log(2 / 2);
+
+let op1 = "hello"
+let op2 = " swapnil"
+let op3 = op1 + op2;
+
+// console.log(op3);
+
+
+let num1, num2, num3 
+
+num1 = num2 = num3 = 2 + 2
+// console.log(num3);
+
+let gameCounter = 100 
+
+++gameCounter
+// console.log(gameCounter);
