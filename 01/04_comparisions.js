5c1efbabd9fe23d215f89aa10e509ad717dce5fa
swapnil0028 <swapnilcodes28@gmail.com>
2025-11-28 21:10:11 +0530

    stack and heap is ocmpleted

--- 01/04_comparisions.js
+++ 01/04_comparisions.js
@@ -23,4 +23,22 @@
 
 // stack(premetive) copy , heap (non-premetive) reference
 
-let myYouTubeNsme = "codeCrafters";
+let myYouTubeName = "codeCrafters";
+
+let anotherYouTubeName = myYouTubeName;
+anotherYouTubeName = "NotCodeCrafters"
+
+console.log(myYouTubeName);
+console.log(anotherYouTubeName);
+
+
+let user1 = {
+    email: "swapnilcodes28@gmail.com",
+     upi: "swapnil@upi"
+}
+
+let user2 = user1;
+user2.email = "hitash@gmail.com"
+
+console.log(user1.email);
+console.log(user2.email);
