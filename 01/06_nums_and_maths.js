be99bc299d3b18bb13e5e8c95d26e307b6d6a97f
swapnil0028 <swapnilcodes28@gmail.com>
2025-11-28 22:50:49 +0530

    num and math

---
+++ 01/06_nums_and_maths.js
@@ -0,0 +1,37 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+// console.log(typeof balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+
+const otherNumvber = 121.12344
+
+// console.log(otherNumvber.toPrecision(3));
+
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+//++++++++++++=========+++=+++++++++====maths+++++++++++++======+++++++++=
+
+// console.log(Math);
+// console.log(Math.abs(-23));
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(40.2));
+// console.log(Math.floor(40.9));
+// console.log(Math.min(4,5,3,2,5,7,4,2,5,7,3));
+// console.log(Math.max(4,5,3,3,6,4,3,6,3,6,3,6,3));
+
+console.log(Math.random());
+console.log((Math.random()*10+1));
+console.log(Math.floor(Math.random()*10+1));
+
+const min = 10 
+const max = 20
+console.log((Math.floor(Math.random() * (max - min + 1))+ min));
