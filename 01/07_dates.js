e6ff6ce8aaa170f90ed264acbc25a30c89b726e1
swapnil0028 <swapnilcodes28@gmail.com>
2025-11-28 23:16:53 +0530

    date half part

---
+++ 01/07_dates.js
@@ -0,0 +1,16 @@
+//Dates
+
+let mydate = new Date();
+// console.log(mydate.toString())
+// console.log(mydate.toDateString());
+// console.log(mydate.toISOString());
+// console.log(mydate.toLocaleDateString());
+// console.log(mydate.toJSON());
+
+
+let mytimestamp = Date.now()
+
+// console.log(mydate.getTime());
+// console.log(mydate.getTime());
+
+console.log(Math.floor(Date.now()/1000));
