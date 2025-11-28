235f3d0cbf30f0dd0fc836b1768da26dc64a67c1
swapnil0028 <swapnilcodes28@gmail.com>
2025-11-28 21:59:40 +0530

    strings and their combinations

---
+++ 01/05_strings.js
@@ -0,0 +1,13 @@
+const name = "swapnil28"
+const myGitRepo = 50;
+
+// console.log(name + myGitRepo + "value");
+
+console.log(`Hellp my name is ${name} and my git repo count is ${myGitRepo}`);
+
+
+const gameName = new String("swapnil007");
+//strings and their combinations 
+
+// using functions like sperating them , sounting them , and combining them with only spaces and other entity , and many more to know, checkout all the functions as needed , and also when you are free .. 
+ 
