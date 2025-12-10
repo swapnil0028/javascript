263d36f46549a1c65c36a49f68accd040e468966
swapnil0028 <swapnilcodes28@gmail.com>
2025-12-10 11:39:44 +0530

    03_scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,39 @@
+function one(){
+    const username = "swapnil"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+        
+    }
+    // console.log(website);
+    
+    // two()
+}
+
+one()
+
+if (true){
+    const username= "swapnil"
+    if(username === "swapnil"){
+        const website= " youtube"
+        // console.log(username + website);
+        
+    }
+    // console.log(website);
+    
+}
+// console.log(username);
+
+
+//+++++++++++++++++++intresting+++++++++++++++++++++
+
+function addone(num){
+    return num + 1
+}
+addone(5)
+ 
+const addtwo = function(num){
+    return num + 2
+}
+addtwo(5)
